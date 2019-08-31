ee7f9fa911a26577aec99d143c4acd96c00d53a2
sontus-tripura <sontustrp.com@gmail.com>
2019-08-31 11:04:46 -0700

    post show dynamically

--- main_app/views.py
+++ main_app/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
+
+
+def post_created(request):
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+    stuff_for_frontend = {'posts':posts}
+    return render (request, 'blog/post_created.html', stuff_for_frontend)
