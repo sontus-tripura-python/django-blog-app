8e892d0b87b09431faca1c2d3fa4159344453760
sontus-tripura <sontustrp.com@gmail.com>
2019-08-31 21:32:26 -0700

    post views details

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
      path('', views.post_created, name='post_created'),
+     path('post/<int:pk>/', views.post_details, name='post_details'),
     ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,7 @@ def post_created(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
     stuff_for_frontend = {'posts':posts}
     return render (request, 'blog/post_created.html', stuff_for_frontend)
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    stuff_for_frontend = { 'posts':post }
+    return render (request, 'blog/post_details.html', stuff_for_frontend)
